a3527ab3a6f9629479cade1facbda0d4e5d911c1
null <mywei1989@gmail.com>
2015-10-28 17:46:08 +0800

    完成api list 最基本功能

--- api/routes/list.js
+++ api/routes/list.js
@@ -17,6 +17,51 @@ module.exports = function(app){
         if(!(err)&&archiveArray){
           res.json(archiveArray);
           res.end();
+        }else{
+          res.json({status:404,message:''});
+          res.end();
+        }
+      });
+    }else{
+      res.end();
+    }
+  });
+
+  app.get('/getPageCount',function(req,res,next){
+    if(req.sessionID){
+      var list = new List({
+        pageIndex:1,
+        pageSize:settings.pageSize,
+        queryObj:{}
+      });
+      list.getCount(function(err,count){
+        if(!(err)&&(count!=0)){
+          res.json(Math.ceil(count/settings.pageSize));
+          res.end();
+        }else{
+          res.json({status:404,message:''});
+          res.end();
+        }
+      });
+    }else{
+      res.end();
+    }
+  });
+
+  app.get('/',function(req,res,next){
+    if(req.sessionID){
+      var list = new List({
+        pageIndex:1,
+        pageSize:settings.pageSize,
+        queryObj:{}
+      });
+      list.getList(function(err,docs){
+        if(!(err)&&docs){
+          res.json(docs);
+          res.end();
+        }else{
+          res.json({status:404,message:''});
+          res.end();
         }
       });
     }else{
