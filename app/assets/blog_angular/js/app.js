ac3c7a311f6eea1ea2ac2764aceaa72f4ea1290e
null <mywei1989@gmail.com>
2015-10-29 23:36:44 +0800

    切分不同的ui-view

--- app/assets/blog_angular/js/app.js
+++ app/assets/blog_angular/js/app.js
@@ -1,1 +1,47 @@
 'use strict';
+
+var blogApp = angular.module('blogApp', [
+  /*'ngRoute',
+  'blogAppControllers'*/
+  'ui.router'
+]);
+
+blogApp
+.config(['$stateProvider', '$urlRouterProvider',
+  function($stateProvider,$urlRouterProvider) {
+    $urlRouterProvider.otherwise("/");
+     $stateProvider
+        .state("index", {
+            url: "/",
+            views:{
+              "list":{
+                templateUrl:"../../partials/list.html",
+                controller:function($scope){
+
+                }
+              },
+              "tags":{
+                templateUrl:"../../partials/tags.html",
+                controller:function($scope){
+
+                }
+              },
+              "archives":{
+                templateUrl:"../../partials/archives.html",
+                controller:function($scope){
+
+                }
+              }
+            }
+        });
+        /*.state("about", {
+            url:"/about",
+            templateUrl: "partials/list.html"
+        })
+        .state("profile", {
+            url:"/profile",
+            templateUrl: "partials/list.html"
+        });*/
+
+
+  }]);
--- app/assets/blog_angular/js/controllers.js
+++ app/assets/blog_angular/js/controllers.js
@@ -1,1 +1,7 @@
 'use strict';
+
+var blogAppControllers = angular.module('blogAppControllers',[]);
+
+blogAppControllers.controller('listCtrl',['$scope','$routeParams','List',function($scope,$routeParams,List){
+
+}]);
